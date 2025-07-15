4fc62ee6e72be89edbe38055b9abfc267766e4e9
Muhammad Surya Putra Davindra <surya@Davindras-MacBook-Pro.local>
2025-07-15 22:51:14 +0700

    new config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://clulijrddwhmsysscmyo.supabase.co' 
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsdWxpanJkZHdobXN5c3NjbXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxODc2ODcsImV4cCI6MjA2NDc2MzY4N30.G4nkA8OKGA_KktCYT67JSLpbUFnxn8V3f0E8M4EuRfE'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
